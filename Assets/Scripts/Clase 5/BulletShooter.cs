329074548cd028371a8c842a7bd7de192f1bd9f2
FranciscoPS <57423154+FranciscoPS@users.noreply.github.com>
2023-04-06 20:14:39 -0600

    Script para instanciar balas

--- Assets/Scripts/Clase 5/BulletShooter.cs
+++ Assets/Scripts/Clase 5/BulletShooter.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BulletShooter : MonoBehaviour
 {
-
+    //Esto es para saber que bala vamos a disparar
+    [SerializeField] private Bullet m_bulletToShoot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shoot();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Shoot()
+    {
+        //Paso 1: Instanciar nueva bala
+        Instantiate(m_bulletToShoot)
     }
 }
